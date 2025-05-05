6edfea8a4f0646872f97ad45a01245907a77b213
davronkhamdamov <xamdamovdavron6@gmail.com>
2025-05-05 20:48:00 +0500

    remove when most common foods not enough

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -11,7 +11,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div class="my-8">
+    <div class="my-8" v-if="items.length >= 3">
         <h1 class="font-bold text-2xl">{{ $t("recently_bought_foods") }}</h1>
         <UCarousel v-slot="{ item }" :items="items" loop arrows dots :autoplay="{ delay: 2500 }"
             :ui="{ item: 'basis-1/2' }" class="rounded-4xl">
