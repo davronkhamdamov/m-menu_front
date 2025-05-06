322a81925b5f35a16d207a9f2e25537cf3934c80
davronkhamdamov <xamdamovdavron6@gmail.com>
2025-05-06 21:31:07 +0500

    send order id when sending feedback

--- pages/order.vue
+++ pages/order.vue
@@ -6,6 +6,7 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 const { locale, t } = useI18n();
+const orderId = ref("")
 
 const goBack = () => {
   router.back();
@@ -66,7 +67,7 @@ const sendOrder = async () => {
     console.error(fetchError.value);
     error.value = "Buyurtma yuborishda xatolik yuz berdi.";
   }
-
+  orderId.value = response.data.id;
   success.value = true;
   modalSuccess.value = true;
   store.clearCart();
@@ -89,11 +90,14 @@ const sendFeedback = async () => {
     star: star.value,
     region: region.value.name,
     feedback: feedback.value,
+    order_id: orderId.value
   });
   if (response.status == 201) {
     isSending.value = false;
     openFeedbackModal.value = false;
     endStatus.value = true;
+  } else {
+    error.value = "Fikringizni yuborishda xatolik yuz berdi.";
   }
 };
 const items = [
@@ -201,11 +205,10 @@ const items = [
             <path fill="green" fill-rule="evenodd"
               d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-1.2a8.8 8.8 0 1 0 0-17.6a8.8 8.8 0 0 0 0 17.6m-1.172-6.242l5.809-5.808l.848.849l-5.95 5.95a1 1 0 0 1-1.414 0L7 12.426l.849-.849l2.98 2.98z" />
           </svg>
-          <p>Bizning xizmatlarimizdan foydalanangiz uchun rahmat 😊</p>
+          <p>Xizmatlarimizdan foydalanganingiz uchun rahmat 😊</p>
         </div>
       </template>
     </UModal>
-
     <p v-if="error" class="text-red-600 mt-4">{{ error }}</p>
   </UContainer>
 </template>
